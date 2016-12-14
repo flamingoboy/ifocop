d5e1afa45e1ed964023765acd4f7d30cfe6eca6a
roro <zefin75@hotmail.fr>
2016-12-14 12:50:34 +0100

    modif2 test.js

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
 alert("Hello");
 
 var thing = function(){
-alert('my thing');
-}
+};
+
+prompt('saise de texe');
+confirm('confirmation');
+alert('message d\'alerte');
+
+var questionUnique = function(texte) {
+  do {
+    var reponse = parseFloat(prompt('Donnez un ' + texte + ' nombre'));
+  } while (isNaN(reponse))
+  return reponse;
+};
+
+var question = function() {
+  alert(questionUnique('premier') + questionUnique('second'));
+};
